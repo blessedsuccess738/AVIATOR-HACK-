6da67d1938f511ff4400fd880734788724282a47
blessedsuccess738 <blessedsuccess738@gmail.com>
2025-11-21 21:14:53 +0100

    Add predict function for random value generation

---
+++ server/services/model.js
@@ -0,0 +1,7 @@
+function predict(input) {
+  // Example stub: always returns a random value
+  // Replace this with your prediction logic or ML model
+  return Math.random();
+}
+
+module.exports = { predict };
